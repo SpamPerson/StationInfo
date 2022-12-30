60dddb73597d6d5a7e8d106068eddff7562429cc
KimChanYoung <nike5539@dotnetsoft.co.kr>
2022-12-30 19:37:32 +0900

    working...

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer: React.FC = () => {
+   return (
+      <div>
+         <hr />
+         Copyright ⓒ SpamPerson. All rights reserved.
+      </div>
+   );
+};
+
+export default Footer;
---
+++ src/components/common/Common.data.ts
@@ -0,0 +1,13 @@
+import { IOption } from "./Common.type";
+
+export const stationLines: IOption[] = [
+    { key: '1', value: '1호선' },
+    { key: '2', value: '2호선' },
+    { key: '3', value: '3호선' },
+    { key: '4', value: '4호선' },
+    { key: '5', value: '5호선' },
+    { key: '6', value: '6호선' },
+    { key: '7', value: '7호선' },
+    { key: '8', value: '8호선' },
+    { key: '9', value: '9호선' },
+ ];
